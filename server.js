bfa7d7bcdd234dcc64385e231e9adbdb270d7465
tauheedkhan <tauheedkhan@outlook.com>
2018-11-13 00:41:36 +0000

    enhanced express setup

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const port = process.env.PORT || 3000;
 const app = express();
 
-
+// DB Config
 const db = require('./config/keys').mongoURI;
 
+// Connect to MongoDB
 mongoose
     .connect(db)
     .then(() => console.log('Mongodb connected'))
@@ -19,3 +24,8 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('Hello API world')
 })
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
