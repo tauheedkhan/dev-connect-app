980975d0ec8823790659d7c16d1f2933df976839
tauheedkhan <tauheedkhan@outlook.com>
2018-11-14 22:47:32 +0000

    added profile endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,72 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
 
-// @route   GET api/profile/test
-// @desc    Test profile route
-// @access  Public
-router.get('/test', (req, res) => res.json({msg: 'profile works'}))
+// Load profile model
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+
+// @route   GET api/profile
+// @desc    Get current user profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const errors = {}
+
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user'
+        return res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
+    .catch(err => res.status(500).json(err))
+})
+
+// @route   POST api/profile
+// @desc    Create/Update user profile
+// @access  Private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const profileFields = {}
+  const errors = {}
+
+  profileFields.user = req.user.id
+  if (req.body.handle) profileFields.handle = req.body.handle
+  if (req.body.company) profileFields.company = req.body.company
+  if (req.body.website) profileFields.website = req.body.website
+  if (req.body.loaction) profileFields.loaction = req.body.loaction
+  if (req.body.bio) profileFields.bio = req.body.bio
+  if (req.body.status) profileFields.status = req.body.status
+  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername
+  if (typeof req.body.skills !== 'undefined') profileFields.skills = req.body.skills.split(',')
+  profileFields.social = {}
+  if (req.body.youtube) profileFields.social.youtube = req.body.youtube
+  if (req.body.twitter) profileFields.social.twitter = req.body.twitter
+  if (req.body.facebook) profileFields.social.facebook = req.body.facebook
+  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
+  if (req.body.instagram) profileFields.social.instagram = req.body.instagram
+
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      if (profile) {
+        // update
+        Profile.findByIdAndUpdate({user: req.user.id}, {$set: profileFields}, {new: true})
+          .then(profile => res.json(profile))
+      }
+      // Create
+
+      // Check handle
+      Profile.findOne({handle: profileFields.handle})
+        .then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exist'
+            res.status(400).json(errors)
+          }
+          // Save profile
+          new Profile(profileFields).save().then(profile => res.json(profile))
+        })
+    })
+})
 
 module.exports = router
